c8a048ead425835737d66b76e459897b247a68f5
BraleyGarafola <130701206+BraleyGarafola@users.noreply.github.com>
2023-10-03 11:21:52 -0500

    adding more to the website

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+function mySecondFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "monday-soap") {
+        x.className += " responsive";
+    } else {
+        x.className = "monday-soap";
+    }
+}
