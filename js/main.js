4a1669948d5a2fe201073a804e211ef365033ffc
BraleyGarafola <130701206+BraleyGarafola@users.noreply.github.com>
2023-10-09 14:05:44 -0500

    still working with media querries

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 // JS scripts placed here
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "flex") {
-      x.style.display = "none";
-  } else {
-      x.style.display = "flex";
-  }
-}
-
 function mySecondFunction() {
   var x = document.getElementById("myTopnav2");
   if (x.className === "header2") {
