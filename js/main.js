16e6bf035887c2cf94066dfe55852e2bedd31d74
BraleyGarafola <130701206+BraleyGarafola@users.noreply.github.com>
2023-10-09 14:24:29 -0500

    still editing querries

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
 // JS scripts placed here
-function mySecondFunction() {
-  var x = document.getElementById("myTopnav2");
-  if (x.className === "header2") {
-      x.className += "responsive";
-  } else {
-      x.className = "header2";
-  }
-}
